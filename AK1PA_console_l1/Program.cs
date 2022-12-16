3ba4bc4f315ad8fe2204ed89a086f10796f7b410
Tomas Trenz <tomas.trenz@gmail.com>
2022-12-16 12:20:45 +0100

    Add project files.

---
+++ AK1PA_console_l1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AK1PA_console_l1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
